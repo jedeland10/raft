70d65b886995fff59b6075f5f8adab5ddec48b8f
jedeland10 <johan.edeland@live.com>
2025-05-14 14:53:05 +0200

    unicache for etcd, entries + committed entries decoded

--- unicache/unicache.go
+++ unicache/unicache.go
@@ -115,11 +115,11 @@ func (uc *uniCache) EncodeData(data []byte) []byte {
 	if ok && id < uc.nextID {
 
 		encodedID := protowire.AppendVarint(nil, uint64(id))
-		newRawPutBytes, err := ReplaceProtoFieldInPlaceCompress(rawPutBytes, cachedFieldNumber, encodedID, protowire.VarintType)
+		newRawPutBytes, err := ReplaceProtoField(rawPutBytes, cachedFieldNumber, encodedID, protowire.VarintType)
 		if err != nil {
 			return data
 		}
-		newData, err := ReplaceProtoFieldInPlaceCompress(data, 4, newRawPutBytes, protowire.BytesType)
+		newData, err := ReplaceProtoField(data, 4, newRawPutBytes, protowire.BytesType)
 		if err != nil {
 			return data
 		}
